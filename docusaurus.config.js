346b093eed47f8c42412a9b914f51b68d8d43033
Yuan.Wang7 <yuan.wang7@caocaoglobal.com>
2023-10-07 17:30:27 +0800

    ci(docusaurus.config.js): 修改配置文件

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
 	projectName: 'react-book', // Usually your repo name.
 	deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
